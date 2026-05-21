6477c59df77b6f34e3384d5fc25fdde7a527d8c8
TuanPh06 <ptun2163@gmail.com>
2026-05-21 11:58:45 +0700

    testing

--- app/routes.py
+++ app/routes.py
@@ -17,11 +17,7 @@ def register_routes(app):
         }), 200
 
     @app.get("/health")
-    def health():
-        import os
-        if os.getenv("FLASK_ENV") == "staging":
-            return jsonify({"status": "error", "message": "Staging crashed!"}), 500
-            
+    def health():   
         return jsonify({"status": "ok"}), 200
 
     @app.get("/api/items")
