95396a393c5ec647f8fc227b7166b063441542b5
TuanPh06 <ptun2163@gmail.com>
2026-05-21 15:50:27 +0700

    Secret

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app
 from app.config import Config
 
 app = create_app()
-
+SECRET_KEY = "day_la_chuoi_bi_mat_admin_@123456_super_secret_key"
 if __name__ == "__main__":
     # Phải truyền host và port vào đây thì Docker mới mở cổng ra ngoài được
     app.run(
